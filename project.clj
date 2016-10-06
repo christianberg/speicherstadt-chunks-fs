2fb6abef0b3c266d0ace02004de3e3cafd4b0679
Christian Berg <berg.christian@gmail.com>
2016-10-06 17:32:48 +0200

    Update dependencies: aero, bidi, yada

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aero "1.0.0"]
+                 [aero "1.0.1"]
                  [aleph "0.4.1"]
-                 [bidi "2.0.10"]
+                 [bidi "2.0.12"]
                  [com.stuartsierra/component "0.3.1"]
                  [me.raynes/fs "1.4.6"]
-                 [yada "1.1.35"]]
+                 [yada "1.1.37"]]
   :main ^:skip-aot speicherstadt.chunks.fs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
