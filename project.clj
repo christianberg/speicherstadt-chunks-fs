6e1e376d496d3471fb99c79299689f58cff9bf73
Christian Berg <berg.christian@gmail.com>
2016-12-23 14:58:19 +0100

    Update dependencies: component, yada

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
                  [aero "1.0.1"]
                  [aleph "0.4.1"]
                  [bidi "2.0.14"]
-                 [com.stuartsierra/component "0.3.1"]
+                 [com.stuartsierra/component "0.3.2"]
                  [me.raynes/fs "1.4.6"]
-                 [yada "1.1.45"]]
+                 [yada "1.1.46"]]
   :main ^:skip-aot speicherstadt.chunks.fs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
