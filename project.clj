2c37953db6e06873df2e7c2ec5a00e26a2b9b0d6
Christian Berg <berg.christian@gmail.com>
2017-01-05 13:49:32 +0100

    Update dependencies: bidi, yada

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [aero "1.0.1"]
                  [aleph "0.4.1"]
-                 [bidi "2.0.14"]
+                 [bidi "2.0.16"]
                  [com.stuartsierra/component "0.3.2"]
                  [me.raynes/fs "1.4.6"]
-                 [yada "1.1.46"]]
+                 [yada "1.2.0"]]
   :main ^:skip-aot speicherstadt.chunks.fs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
